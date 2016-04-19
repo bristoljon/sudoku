102865a685159bacb6c278981f9f21cc1f9cbc37
Jon Wyatt <jon@bristoljon.uk>
2016-04-19 11:34:55 +0100

    [IMP] Browserified and Babelified the ES6 into ES5 for backward compatibility

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var source = require('vinyl-source-stream');
+var browserify = require('browserify');
+var watchify = require('watchify');
+var babelify = require('babelify');
+
+gulp.task('default', function() {
+  var bundler = watchify(browserify({
+    entries: ['./script.js'],
+    transform: [babelify],
+    debug: true,
+    cache: {},
+    packageCache: {},
+    fullPaths: true
+  }));
+
+  function build(file) {
+    if (file) gutil.log('Recompiling ' + file);
+    return bundler
+      .bundle()
+      .on('error', gutil.log.bind(gutil, 'Browserify Error'))
+      .pipe(source('main.js'))
+      .pipe(gulp.dest('./'));
+  };
+  build();
+  bundler.on('update', build);
+});
